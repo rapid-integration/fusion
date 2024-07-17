5dd15f693d2d4667993ba06d6ba78133c0fe066e
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2024-07-18 02:13:03 +0300

    refactoring

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -5,9 +5,9 @@ from src.api.v1 import auth, uploads, users, verify
 v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(auth.router)
+v1_router.include_router(verify.router)
 v1_router.include_router(users.router)
 v1_router.include_router(uploads.router)
-v1_router.include_router(verify.router)
 
 __all__ = [
     "v1_router",
