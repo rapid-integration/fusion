781065c6e30d7893c7ea1cf021604275cbf6fbc6
zobweyt <zobweyt@gmail.com>
2024-07-17 20:24:30 +0300

    Add protected & unprotected routes

---
+++ frontend/src/routes/(unauthorized).tsx
@@ -0,0 +1,9 @@
+import { RouteSectionProps } from "@solidjs/router";
+import { Show } from "solid-js";
+import { createRedirectAuthorized } from "~/lib/auth";
+
+export default function Unauthorized(props: RouteSectionProps) {
+  const isRedirected = createRedirectAuthorized("/");
+
+  return <Show when={!isRedirected()}>{props.children}</Show>;
+}
