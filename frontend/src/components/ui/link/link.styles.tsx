21313bb9b702e3756bbe526927231dfd1e398ae9
zobweyt <zobweyt@gmail.com>
2024-08-01 22:18:33 +0300

    Add link component with initial styling

---
+++ frontend/src/components/ui/link/link.styles.tsx
@@ -0,0 +1,12 @@
+import { tv } from "tailwind-variants";
+
+export const styles = tv({
+  base: [
+    "text-blue-600 text-sm cursor-pointer",
+    "hover:text-blue-700 hover:underline active:text-blue-800",
+    "transition-color duration-75",
+    "focus-visible:ring active:hover:duration-0",
+    "aria-disabled:cursor-not-allowed aria-disabled:opacity-75 disabled:cursor-not-allowed disabled:opacity-75",
+    "aria-busy:cursor-progress",
+  ],
+});
