d1fe1252a106c0e09b24763a994f1d707156c649
zobweyt <zobweyt@gmail.com>
2024-08-08 20:47:46 +0300

    Add better API logging

--- frontend/src/lib/api/middleware.ts
+++ frontend/src/lib/api/middleware.ts
@@ -1,4 +1,5 @@
 import { type Middleware } from "openapi-fetch";
+import colors from "picocolors";
 import { getSession, resetSession } from "~/lib/auth/session";
 
 export const AUTH_MIDDLEWARE: Middleware = {
@@ -19,6 +20,12 @@ export const AUTH_MIDDLEWARE: Middleware = {
 
 export const DEBUG_MIDDLEWARE: Middleware = {
   onResponse: (options) => {
-    console.debug(`${options.request.method} ${options.schemaPath} - ${options.response.status} ${options.response.statusText}`);
+    const timestamp = colors.dim(new Date().toLocaleTimeString());
+    const prefix = colors.cyan(colors.bold("[api]"));
+    const method = colors.green(options.request.method);
+    const route = colors.dim(options.request.url);
+    const status = colors.yellow(`${options.response.status} ${options.response.statusText}`);
+
+    console.debug(`${timestamp} ${prefix} ${method} ${route} ${status}`);
   },
 };
