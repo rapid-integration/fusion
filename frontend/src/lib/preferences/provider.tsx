52f9d9709ace5b70c69e5a64be2fddf6340a9069
zobweyt <zobweyt@gmail.com>
2024-08-20 14:30:37 +0300

    Properly handle mount and cleanup

--- frontend/src/lib/preferences/provider.tsx
+++ frontend/src/lib/preferences/provider.tsx
@@ -1,5 +1,5 @@
 import * as storage from "@solid-primitives/storage";
-import { type ParentComponent, createContext, createEffect, useContext } from "solid-js";
+import { type ParentComponent, createContext, createEffect, onCleanup, onMount, useContext } from "solid-js";
 import { createStore, SetStoreFunction } from "solid-js/store";
 import { type Settings, getDefaultSettings, PREFERENCES_COOKIE_OPTIONS } from "~/lib/preferences";
 
@@ -13,11 +13,25 @@ export const SYNC_BROADCAST_CHANNEL_NAME = "preferences_sync" as const;
 export const PreferencesContext = createContext<PreferencesContextValue>({} as PreferencesContextValue);
 
 export const PreferencesProvider: ParentComponent = (props) => {
-  const channel = new BroadcastChannel(SYNC_BROADCAST_CHANNEL_NAME);
+  const syncBroadcastChannel = new BroadcastChannel(SYNC_BROADCAST_CHANNEL_NAME);
   const [settings, set] = storage.makePersisted(createStore(getDefaultSettings()), PREFERENCES_COOKIE_OPTIONS);
 
-  channel.onmessage = (event) => set(event.data);
-  createEffect(() => channel.postMessage({ ...settings }), { defer: true });
+  onMount(() => {
+    syncBroadcastChannel.onmessage = (event) => {
+      set(event.data);
+    };
+  });
+
+  createEffect(
+    () => {
+      syncBroadcastChannel.postMessage({ ...settings });
+    },
+    { defer: true },
+  );
+
+  onCleanup(() => {
+    syncBroadcastChannel.close();
+  });
 
   return <PreferencesContext.Provider value={{ settings, set }}>{props.children}</PreferencesContext.Provider>;
 };
